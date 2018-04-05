62a2a787f2eb5f685f7e41f35594264055ccdd61
Fumiya Kume <fumiya.kume@hotmail.com>
2018-04-06 00:04:08 +0900

    チュートリアル画面とか作った

--- KeyaLens/KeyaLens/ViewModels/CameraPageViewModel.cs
+++ KeyaLens/KeyaLens/ViewModels/CameraPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using System.Reactive.Linq;
+using Prism.Mvvm;
 using keyalens.Usecase;
 using Reactive.Bindings;
 
@@ -10,6 +11,7 @@ namespace keyalens.ViewModels
 
         public ReactiveProperty<string> TakePhotoUrl { get; set; }
         public ReadOnlyReactivePropertySlim<string> Predictname { get; set; }
+        public ReadOnlyReactivePropertySlim<bool> HasPredictionResult { get; set; }
 
         public ReactiveCommand TakePhotoCommand { get; set; } = new ReactiveCommand();
         public CameraPageViewModel(IFaceAnalyzeUsecase faceAnalyzeUsecase)
@@ -19,6 +21,8 @@ namespace keyalens.ViewModels
             TakePhotoUrl = _faceAnalyzeUsecase.TakeImageUrl;
 
             Predictname = _faceAnalyzeUsecase.PredictName.ToReadOnlyReactivePropertySlim();
+
+            HasPredictionResult = Predictname.Select(s => !string.IsNullOrEmpty(s)).ToReadOnlyReactivePropertySlim();
 
             TakePhotoCommand.Subscribe(() => { _faceAnalyzeUsecase.Analyze(); });
         }
--- KeyaLens/KeyaLens/ViewModels/MainPageViewModel.cs
+++ KeyaLens/KeyaLens/ViewModels/MainPageViewModel.cs
@@ -5,15 +5,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using keyalens.Views;
+using Reactive.Bindings;
 
 namespace keyalens.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+
+        public ReactiveCommand NavigateCameraPage { get; set; } = new ReactiveCommand();
+
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
+            _navigationService = navigationService;
             Title = "Main Page";
+
+            NavigateCameraPage
+                .Subscribe(() => _navigationService.NavigateAsync(nameof(CameraPage)));
         }
     }
 }
